e3400feff94df960fcc8bb7291833f1db0782813
Derrick Kyereh <69907881+kyerehdi@users.noreply.github.com>
2024-03-11 10:40:13 -0400

    almost

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -1,13 +1,9 @@
 import { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
-  appId: 'com.example.app',
+  appId: 'com.prime.TrackAndTrain.app',
   appName: 'fitness-app',
   webDir: 'www',
-  server: {
-    url: 'http://192.168.1.162:4200',
-    cleartext: true,
-  },
 };
 
 export default config;
